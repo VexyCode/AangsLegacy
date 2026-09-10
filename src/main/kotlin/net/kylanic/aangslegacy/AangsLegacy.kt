bffc0dc576b45f00dea885438c9eda78ad25b070
Vexor <filxapcastravete@gmail.com>
2026-09-10 17:13:38 +0300

    first commit

---
+++ src/main/kotlin/net/kylanic/aangslegacy/AangsLegacy.kt
@@ -0,0 +1,14 @@
+package net.kylanic.aangslegacy
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AangsLegacy : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
